a8d4394bf69379663404691903faa3a2f2d5fa8c
Kiritow <1362050620@qq.com>
2023-06-09 22:05:28 +0000

    allow ospf traffic by default

--- app.py
+++ app.py
@@ -156,7 +156,7 @@ def try_insert_iptables_rule(table_name, chain_name, rule_args):
 
         logger.info('iptables rule not exist, inserting: iptables -t {} -I {} {}'.format(table_name, chain_name, rule_args))
         sudo_call(["iptables", "-t", table_name, "-I", chain_name] + rule_args)
-        
+
 
 def try_flush_iptables(table_name, chain_name):
     try:
@@ -272,6 +272,9 @@ def config_up(parser: NetworkConfigParser):
     if parser.enable_local_network and parser.local_is_exit_node:
         try_append_iptables_rule("nat", f"{parser.namespace}-POSTROUTING", ["-o", parser.local_interface.name, "-j", "MASQUERADE"])
         try_append_iptables_rule("filter", f"{parser.namespace}-FORWARD", ["-i", "{}0".format(parser.local_veth_prefix), "-o", parser.local_interface.name, "-j", "ACCEPT"])
+
+    if parser.enable_local_network and parser.local_interface.enable_ospf:
+        try_append_iptables_rule("filter", f"{parser.namespace}-INPUT", ["-p", "ospf", "-j", "ACCEPT"])
 
     # Network mapping
     if parser.enable_local_network and parser.local_network_mapping:
