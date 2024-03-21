283f4ae8474f88a1af9e0080bc5740a606b5a928
Kiritow <1362050620@qq.com>
2024-03-21 19:34:21 +0000

    fix pingcost tools

--- tools_pingcost.py
+++ tools_pingcost.py
@@ -1,13 +1,16 @@
 import os
 import json
+import math
 from common.ping import check_direct_ping, get_peer_ip
+from common.bird import simple_format
 
 
 def render_single_hint(hint_tag, ping_data):
     skip = 0
     if 'skips' in hint_tag:
-        skip = hint_tag['skip']
-
+        skip = hint_tag['skips']
+        
+    interface_name = hint_tag['interface']
     if interface_name not in ping_data:
         return False, None
 
@@ -22,8 +25,8 @@ def render_hint_pingcost(content, ping_data):
             skip -= 1
             continue
 
+        output.append(line)
         if not line.startswith('#HINT:'):
-            output.append(line)
             continue
 
         hint_tag = json.loads(line.replace('#HINT: ', ''))
@@ -35,7 +38,7 @@ def render_hint_pingcost(content, ping_data):
         if not is_valid:
             output.append(line)
             continue
-        
+
         skip, next_lines = hint_data
         output.extend(next_lines)
 
@@ -46,7 +49,7 @@ if __name__ == "__main__":
     NETWORK_NAMESPACE = os.getenv('NETWORK_NAMESPACE') or ''
     INTERFACE_LIST = os.getenv("INTERFACE_LIST") or ''
     INPUT_CONFIG = os.getenv('INPUT_CONFIG') or ''
-    
+
     if not NETWORK_NAMESPACE or not INTERFACE_LIST or not INPUT_CONFIG:
         print('missing env vars')
         exit(1)
@@ -55,16 +58,17 @@ if __name__ == "__main__":
     ping_data = {}
     for interface_name in interfaces:
         peer_ip = get_peer_ip(NETWORK_NAMESPACE, interface_name)
-        ping_result = check_direct_ping(NETWORK_NAMESPACE, peer_ip, ping_count=5)
-        if ping_result < 1:
+        ping_us = check_direct_ping(NETWORK_NAMESPACE, peer_ip, ping_count=5)
+        if ping_us < 1:
             continue
-        ping_data[interface_name] = ping_result
+        ping_data[interface_name] = int(math.ceil(ping_us // 1000))
 
     with open(INPUT_CONFIG) as f:
         content = f.read()
-    
+
     content = render_hint_pingcost(content, ping_data)
-    
+    content = simple_format(content)
+
     # with open(INPUT_CONFIG, 'w') as f:
     #     f.write(content)
     print(content)
