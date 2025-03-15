3c3ffef077e4aedcd9581e8d426a6360c797b94d
Kiritow <1362050620@qq.com>
2025-03-15 10:17:20 +0000

    add type hint to podman

--- common/podman.py
+++ common/podman.py
@@ -4,7 +4,7 @@ from .utils import sudo_call, sudo_call_output, get_tempdir_path
 from .utils import logger
 
 
-def inspect_podman_router(namespace):
+def inspect_podman_router(namespace: str):
     container_name = "{}-router".format(namespace)
 
     container_list = sudo_call_output(["podman", "ps", "-a", "--format=json"])
@@ -19,7 +19,7 @@ def inspect_podman_router(namespace):
             return container_inspect_result[0]
 
 
-def shutdown_podman_router(namespace):
+def shutdown_podman_router(namespace: str):
     container_inspect_result = inspect_podman_router(namespace)
     if not container_inspect_result:
         return
@@ -34,7 +34,7 @@ def shutdown_podman_router(namespace):
 
 
 
-def start_podman_router(namespace):
+def start_podman_router(namespace: str):
     logger.info('starting router with namespace {}'.format(namespace))
     sudo_call(["podman", "run", "--network", "ns:/var/run/netns/{}".format(namespace), 
                "--cap-add", "NET_ADMIN", "--cap-add", "CAP_NET_BIND_SERVICE", "--cap-add", "NET_RAW", "--cap-add", "NET_BROADCAST",
